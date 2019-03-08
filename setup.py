cadc7759ba003ece4a336bdc03048a11597d1bbb
gabrieldemarmiesse <gabreil.de-marmiesse@valeo.com>
2019-03-08 17:10:51 +0100

    works well with just the setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 import sys
 from sultan import Sultan
 import pathlib
-
+import os
+import torch
+from torch.utils.ffi import create_extension
 
 CUDA_ARCH = ('-gencode arch=compute_30,code=sm_30 '
              '-gencode arch=compute_35,code=sm_35 '
@@ -14,7 +16,7 @@ CUDA_ARCH = ('-gencode arch=compute_30,code=sm_30 '
 
 project_dir = pathlib.Path(__file__).resolve().parent
 finished_file = project_dir / 'finished_install.txt'
-
+src = project_dir / 'retinanet/lib/nms/src'
 
 if not finished_file.exists():
     with Sultan.load(cwd=project_dir / 'retinanet/lib/nms/src/cuda') as s:
@@ -23,10 +25,29 @@ if not finished_file.exists():
                'cu -Xcompiler -fPIC ' + CUDA_ARCH)
         s.bash(f'-c "{cmd}"').run()
 
-#    with Sultan.load(cwd=project_dir / 'retinanet/lib/nms') as s:
-#        s.bash(sys.executable + ' build.py').run(streaming=True)
-    finished_file.touch()
+    sources = [src / 'nms.c']
+    headers = [src / 'nms.h']
+    defines = []
+    with_cuda = False
 
+    if torch.cuda.is_available():
+        print('Including CUDA code.')
+        sources += [src / 'nms_cuda.c']
+        headers += [src / 'nms_cuda.h']
+        defines += [('WITH_CUDA', None)]
+        with_cuda = True
+
+    extra_objects = [src / 'cuda/nms_kernel.cu.o']
+    ffi = create_extension('retinanet.lib.nms._ext.nms',
+                           headers=[str(x) for x in headers],
+                           sources=[str(x) for x in sources],
+                           define_macros=defines,
+                           relative_to=__file__,
+                           with_cuda=with_cuda,
+                           extra_objects=[str(x) for x in extra_objects],
+                           extra_compile_args=['-std=c99'])
+    ffi.build()
+    finished_file.touch()
 
 setup(name='retinanet',
       packages=find_packages())
